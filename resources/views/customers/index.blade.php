ea998242419e63e7001c3fa077f6b9a65abdeeea
Pawel Damrath <pawel.damrath@student.po.edu.pl>
2021-06-20 12:24:31 +0200

    dopracys

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,8 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+#map-canvas {
+  width: 100%;
+  height: 400px;
+}
+</style>
+<script>
+function myFunction() {
+  var x = document.getElementById("table");
+    x.style.display = "table";
+}
+//https://www.codeply.com/go/ZrKScrDvuE/bootstrap-load-google-map-inside-modal
+//https://www.codeply.com/go/fJjRC7nujr/bootstrap-4-google-map-in-modal
+//https://mdbootstrap.com/docs/b4/jquery/javascript/google-maps/
+var element = $(this);
+var map;
+function initialize(myCenter) {
+  var marker = new google.maps.Marker({
+    position: myCenter
+  });
+
+  var mapProp = {
+    center: myCenter,
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapProp);
+  marker.setMap(map);
+};
+
+$('#myMapModal').on('show.bs.modal', function(e) {
+  var element = $(e.relatedTarget);
+  var data = element.data("lat").split(',');
+  var latlng = new google.maps.LatLng(data[0], data[1]);
+  initialize(latlng);
+  $("#lat").html(latlng.lat() + ", " +latlng.lng());
+  google.maps.event.trigger(map, 'resize');
+});
+</script>
+
         <!-- Portfolio Section-->
-        <section class="masthead page-section portfolio" id="portfolio">
+        <section class="masthead page-section portfolio" id="portfolio" >
             <div class="container">
 
         @if(session()->has('message'))
@@ -19,7 +60,10 @@
                     <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                     <div class="divider-custom-line"></div>
                 </div>
-                <table class="table table-striped table-dark">
+
+                <button type="button" class="btn btn-primary" onclick="myFunction()">Pokaż tabele</button>
+                <a href="#" data-lat="-15,25" data-toggle="modal" data-target="#myMapModal" class="btn btn-primary">Map 1</a>
+                <table class="table table-striped table-dark" id="table" STYLE="display:none">
   <thead>
     <tr>
       <th scope="col">#</th>
@@ -42,6 +86,29 @@
     @endforeach
   </tbody>
 </table>
+
             </div>
+            <div class="modal fade" id="myMapModal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title w-100">Google Map <span id="lat" class="float-right"></span></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            </div>
+            <div class="modal-body">
+                <div id="map-canvas" class="">
+                
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDAuTFPlEcDfHzqTWBaSzfaGPWIJpyOUTk&callback=myMap"></script>
         </section>
 @endsection   
